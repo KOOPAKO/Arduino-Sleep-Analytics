a320a150b9269f72ef84224a126c4f6ce62d1058
Cooper <50361120+KOOPAKO@users.noreply.github.com>
2020-09-08 17:40:29 +1000

    added capability for viewing previous files
    and UI for stopping and starting recording

--- static/js/refreshValues.js
+++ static/js/refreshValues.js
@@ -1,16 +1,17 @@
 function refresh_values(){
-    $.ajax({
-        url: "/_refresh_data/",
-        success: function(output){
-            $("#millis").html(JSON.stringify(output["millis"]).split("\"").join(""));
-            $("#stamp").html(JSON.stringify(output["stamp"]).split("\"").join(""));
-            $("#datetime").html(JSON.stringify(output["datetime"]).split("\"").join(""));
-            $("#motion").html(JSON.stringify(output["motion"]).split("\"").join(""));
-            $('#temperature').html(JSON.stringify(output["temp"]).split("\"").join(""));
-            $('#humidity').html(JSON.stringify(output["hum"]).split("\"").join(""));
-            
-            setTimeout(refresh_values, 1000)
-        }
-    })
+    if(recording == 1){
+        $.ajax({
+            url: "/_refresh_data/",
+            success: function(output){
+                $("#millis").html(JSON.stringify(output["millis"]).split("\"").join(""));
+                $("#stamp").html(JSON.stringify(output["stamp"]).split("\"").join(""));
+                $("#datetime").html(JSON.stringify(output["datetime"]).split("\"").join(""));
+                $("#motion").html(JSON.stringify(output["motion"]).split("\"").join(""));
+                $('#temperature').html(JSON.stringify(output["temp"]).split("\"").join(""));
+                $('#humidity').html(JSON.stringify(output["hum"]).split("\"").join(""));
+            }
+        })
+    }
+    setTimeout(refresh_values, 1000)
 }
 refresh_values()
